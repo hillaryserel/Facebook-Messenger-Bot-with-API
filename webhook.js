2ab9fb81867e289d90eeddee8b5ae761f31c59ef
Hillary Serel <Serelhillary00@gmail.com>
2024-06-05 17:09:59 +0300

    Initial commit

---
+++ webhook.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 4000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+/* For Facebook Validation */
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] && req.query['hub.verify_token'] === `"abcdefgh12345678"`) {
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      res.status(403).end();
+    }
+  });
+  
+  /* Handling all messenges */
+  app.post('/webhook', (req, res) => {
+    console.log(req.body);
+    if (req.body.object === 'page') {
+      req.body.entry.forEach((entry) => {
+        entry.messaging.forEach((event) => {
+          if (event.message && event.message.text) {
+            sendMessage(event);
+          }
+        });
+      });
+      res.status(200).end();
+    }
+  });
